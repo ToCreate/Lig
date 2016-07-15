0094a7098a06edaa5c90cc48eb95314730254859
prf <1550749301@qq.com>
2016-07-15 17:15:23 +0800

    RCP service

--- src/main/java/com/light/rpc/service/impl/HelloServiceImpl.java
+++ src/main/java/com/light/rpc/service/impl/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package com.light.rpc.service.impl;
 
 import com.light.rpc.service.HelloService;
+import com.light.rpc.service.RpcService;
 
 @RpcService(HelloService.class) // 指定远程接口
 public class HelloServiceImpl implements HelloService {
